eaa1030c0a236d0a99fcad5817f3506a4b7f9e75
Thijs Kuilman <thijs.kuilman@gmail.com>
2025-01-10 20:10:51 +0100

    Added Mention Node class

---
+++ src/Extensions/Nodes/Mention.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FilamentTiptapEditor\Extensions\Nodes;
+
+use Tiptap\Core\Node;
+
+class Mention extends Node
+{
+    public static $name = 'mention';
+}
