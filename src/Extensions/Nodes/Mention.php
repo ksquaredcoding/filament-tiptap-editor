e135f1969345928269ff0f865e5475bd897acdb4
Thijs Kuilman <thijs.kuilman@gmail.com>
2025-01-13 10:37:44 +0100

    Added HTML rendering of Mention node

--- src/Extensions/Nodes/Mention.php
+++ src/Extensions/Nodes/Mention.php
@@ -9,4 +9,50 @@ use Tiptap\Core\Node;
 class Mention extends Node
 {
     public static $name = 'mention';
+
+    public function addOptions(): array
+    {
+        return [
+            'suggestion' => [
+                'char' => '@',
+            ],
+        ];
+    }
+
+    public function addAttributes(): array
+    {
+        return [
+            'href' => [
+                'default' => null,
+                'parseHTML' => function ($DOMNode) {
+                    return $DOMNode->firstChild->getAttribute('src');
+                },
+            ],
+        ];
+    }
+
+    public function parseHTML(): array
+    {
+        return [
+            [
+                'tag' => 'span[data-mention]',
+            ],
+        ];
+    }
+
+    public function renderText($node)
+    {
+        return $node->attrs->label;
+    }
+
+    public function renderHTML($node, $HTMLAttributes = []): array
+    {
+        return [
+            'span',
+            [
+                'data-mention-id' => $node->attrs->id,
+            ],
+            0,
+        ];
+    }
 }
