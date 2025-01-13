6ce91ba095bc5c4cdac0bc581d1bfe30bd1c9891
Thijs Kuilman <thijs.kuilman@gmail.com>
2025-01-13 15:32:08 +0100

    Added loading states, placeholder in getContent

--- resources/js/extensions/Mention/get-content.js
+++ resources/js/extensions/Mention/get-content.js
@@ -1,7 +1,12 @@
-export default (props) => {
+export default (props, noSuggestionsFoundMessage, suggestionsPlaceholder, query, suggestAfterTyping) => {
     Alpine.data('suggestions', () => ({
         items: props.items,
+        query: query,
         selectedIndex: 0,
+        noSuggestionsFoundMessage: noSuggestionsFoundMessage,
+        suggestionsPlaceholder: suggestionsPlaceholder,
+        loading: false,
+        suggestAfterTyping: suggestAfterTyping,
         init() {
             this.$watch('items', () => {
             });
@@ -10,8 +15,17 @@ export default (props) => {
             ['@update-props.window'](e) {
                 this.items = e.detail.items || [];
             },
+            ['@update-mention-query.window'](e) {
+                this.query = e.detail.query || '';
+            },
             ['@suggestion-keydown.window.stop'](e) {
                 this.onKeyDown(e.detail);
+            },
+            ['@mention-api-loading-start.window'](e) {
+                this.loading = true;
+            },
+            ['@mention-api-loading-end.window'](e) {
+                this.loading = false;
             },
         },
 
@@ -43,12 +57,23 @@ export default (props) => {
         },
     }));
     return `
-  <div class="mention-dropdown" x-data=suggestions  x-bind="rootEvents" x-trap="true">
+  <div class="mention-dropdown" x-data=suggestions  x-bind="rootEvents">
   <template x-for="(item, index) in items">
-  <p :class="{'selected': selectedIndex === index}" @click="selectItem(index)" x-text="item['label']"></p>
+  <p :class="{'selected': selectedIndex === index}" @click="selectItem(index)" x-text="item['label']" x-show="!loading"></p>
   </template>
-  <template x-if="! items.length">
-    <p>No results found</p>
+<!--  No results found -->
+  <template x-if="! items.length && !loading && (query.length || !suggestAfterTyping)">
+      <p x-text="noSuggestionsFoundMessage"></p>
+  </template>
+<!--  Placeholder-->
+    <template x-if="suggestAfterTyping && ! items.length && ! query.length && !loading">
+      <p x-text="suggestionsPlaceholder"></p>
+  </template>
+  <template x-if="loading">
+    <svg class="animate-spin mr-3 size-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
   </template>
   </div>
   `;
