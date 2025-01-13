f89f5e0182bb215eaf4f1f5e64eb486cc4e07424
Thijs Kuilman <thijs.kuilman@gmail.com>
2025-01-13 15:32:38 +0100

    Added debounce, suggestAfterTyping, translation messages to HasMentions

--- src/Concerns/HasMentions.php
+++ src/Concerns/HasMentions.php
@@ -17,6 +17,19 @@ trait HasMentions
         'Content-Type' => 'application/json',
     ];
 
+    protected int $mentionApiDebounce = 250;
+
+    protected bool | Closure | null $suggestAfterTyping = false;
+
+    protected string | Closure | null $noSuggestionsFoundMessage = null;
+
+    protected string | Closure | null $suggestionsPlaceholder = null;
+
+    /**
+     * Set mention suggestions.
+     *
+     * @param  array|Closure|null  $suggestions  Either a hardcoded array or an array with MentionItem objects
+     */
     public function mentionItems(array | Closure | null $suggestions): static
     {
         $this->mentionItems = $suggestions;
@@ -36,7 +49,14 @@ trait HasMentions
             ->toArray();
     }
 
-    public function mentionItemsApi(string | Closure | null $endpoint, ?array $body = null, ?array $headers = null): static
+    /**
+     * Retrieve mention suggestions through an API endpoint
+     *
+     * @param  string|Closure|null  $endpoint  The API endpoint
+     * @param  array|null  $body  Optional body data for the API request. Will always contain a query param.
+     * @param  array|null  $headers  Optional headers for the API request. Is application/json by default.
+     */
+    public function mentionItemsApi(string | Closure | null $endpoint, ?array $body = null, ?array $headers = null, int $debounce = 250): static
     {
         $this->mentionApiEndpoint = $endpoint;
 
@@ -48,6 +68,8 @@ trait HasMentions
             'Content-Type' => 'application/json',
             'X-CSRF-TOKEN' => csrf_token(),
         ];
+
+        $this->mentionApiDebounce = $debounce;
 
         return $this;
     }
@@ -65,5 +87,55 @@ trait HasMentions
     public function getMentionApiHeaders(): array
     {
         return $this->mentionApiHeaders;
+    }
+
+    public function getMentionApiDebounce(): int
+    {
+        return $this->mentionApiDebounce;
+    }
+
+    /**
+     * Set whether suggestions should only appear after you start typing
+     */
+    public function suggestAfterTyping(bool | Closure | null $suggestAfterTyping = true): static
+    {
+        $this->suggestAfterTyping = $suggestAfterTyping;
+
+        return $this;
+    }
+
+    public function getSuggestAfterTyping(): bool
+    {
+        return $this->evaluate($this->suggestAfterTyping);
+    }
+
+    /**
+     * Set the message to display when no mention suggestions are found.
+     */
+    public function noSuggestionsFoundMessage(string | Closure | null $message): static
+    {
+        $this->noSuggestionsFoundMessage = $message;
+
+        return $this;
+    }
+
+    public function getNoSuggestionsFoundMessage(): string
+    {
+        return $this->evaluate($this->noSuggestionsFoundMessage) ?? trans('filament-tiptap-editor::editor.mentions.no_suggestions_found');
+    }
+
+    /**
+     * Set the message to display in the empty suggestions state when the trigger character is typed but no input is provided.
+     */
+    public function suggestionsPlaceholder(string | Closure | null $message): static
+    {
+        $this->suggestionsPlaceholder = $message;
+
+        return $this;
+    }
+
+    public function getSuggestionsPlaceholder(): string
+    {
+        return $this->evaluate($this->suggestionsPlaceholder) ?? trans('filament-tiptap-editor::editor.mentions.suggestions_placeholder');
     }
 }
